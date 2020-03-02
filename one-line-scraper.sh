3e5d93dcee285a08827c6cd41cbc98715f296fd1
V S Sreejeet <vsreejeet@gmail.com>
2020-03-02 12:55:54 +0530

    Update one-line-scraper.sh

--- one-line-scraper.sh
+++ one-line-scraper.sh
@@ -1,1 +1,1 @@
-curl https://www.free-proxy-list.net/ | gawk 'RS="<tr>" { match($0, "([0-9]+\\.){3}[0-9]+</td><td>[0-9]+", ip) } { gsub("</td><td>", ":", ip[0]) } { if (ip[0]) print ip[0] }' > proxies.txt
+curl https://free-proxy-list.net | awk 'BEGIN{RS="<tr>"; FS="</td>"}; {gsub("<td>","")}; {if ($5=="elite proxy") {print ($1":"$2)}}' > proxies.txt
